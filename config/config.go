7c320179275a006fdbb373407728fd16ccd4d441
depengli <ldpfieldlee@gmail.com>
2018-11-28 17:18:39 +0800

    fix online path

--- config/config.go
+++ config/config.go
@@ -6,28 +6,28 @@ package config
 
 import (
 	"asyncTask/model"
-	"log"
-	"io/ioutil"
 	"encoding/json"
+	"io/ioutil"
+	"log"
 )
 
 const (
-	ACTION_PRINT = 1
+	ACTION_PRINT           = 1
 	ACTION_RECONNECT_MYSQL = 2
-	DOCKER_EXEC_START = "0"
-	DOCKER_EXEC_STOP = "1"
-	DOCKER_EXEC_RESTART = "2"
-	DOCKER_EXEC_BACKUP = "3"
+	DOCKER_EXEC_START      = "0"
+	DOCKER_EXEC_STOP       = "1"
+	DOCKER_EXEC_RESTART    = "2"
+	DOCKER_EXEC_BACKUP     = "3"
 )
 
-var __config * model.Config
+var __config *model.Config
 
-func All() * model.Config{
+func All() *model.Config {
 	return __config
 }
 
 func InitConf(args []string) {
-	var confFile = "C:/work/fanxu/go/src/asyncTask/config/online.json"
+	var confFile = "/var/config/online.json"
 	for i := 1; i < len(args); i++ {
 		switch args[i] {
 		case "-f":
@@ -46,4 +46,3 @@ func InitConf(args []string) {
 		log.Fatalln(err.Error(), "cannot parse the file: online.json")
 	}
 }
-
